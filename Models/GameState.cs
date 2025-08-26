6802f76c5f6b767d7ba373affc12ed7ffbdce487
ほたまちスバル <93361361+minamikana-git@users.noreply.github.com>
2025-08-26 19:30:30 +0900

    worked

--- Models/GameState.cs
+++ Models/GameState.cs
@@ -1,33 +1,34 @@
-using HanafudaAdvisor.Wpf.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-public sealed class GameState
+namespace HanafudaAdvisor.Wpf.Models
 {
-    // Public info at any time
-    public List<Card> MyHand { get; } = new(); // 8
-    public List<Card> OppKnown { get; } = new(); // if any revealed
-    public List<Card> Field { get; } = new(); // 8 at start
-    public List<Card> MyPile { get; } = new();
-    public List<Card> OppPile { get; } = new();
-    public HashSet<Card> Discarded { get; } = new(); // dead/killed if variant
-
-
-    public IEnumerable<Card> Seen => MyHand.Concat(OppKnown).Concat(Field).Concat(MyPile).Concat(OppPile).Concat(Discarded);
-    public IEnumerable<Card> Unknown => Deck.Full.Except(Seen);
-
-
-    public int RemainingOfMonth(int month)
-    => Deck.Full.Count(c => c.Month == month) - Seen.Count(c => c.Month == month);
-
-
-    public GameState CloneShallow()
+    public sealed class GameState
     {
-        var g = new GameState();
-        g.MyHand.AddRange(MyHand);
-        g.OppKnown.AddRange(OppKnown);
-        g.Field.AddRange(Field);
-        g.MyPile.AddRange(MyPile);
-        g.OppPile.AddRange(OppPile);
-        g.Discarded.UnionWith(Discarded);
-        return g;
+        public List<Card> MyHand { get; } = new();   // ©•ª‚ÌèD
+        public List<Card> OppKnown { get; } = new(); // ‘Šè‚ÌŒöŠJî•ñi”»–¾Dj
+        public List<Card> Field { get; } = new();    // ê
+        public List<Card> MyPile { get; } = new();   // ©•ª‚ªæ‚Á‚½D
+        public List<Card> OppPile { get; } = new();  // ‘Šè‚ªæ‚Á‚½D
+
+        public IEnumerable<Card> Seen =>
+            MyHand.Concat(OppKnown).Concat(Field).Concat(MyPile).Concat(OppPile);
+
+        public IEnumerable<Card> Unknown =>
+            Deck.Full.Except(Seen);
+
+        public int RemainingOfMonth(int month) =>
+            Deck.Full.Count(c => c.Month == month) - Seen.Count(c => c.Month == month);
+
+        public GameState CloneShallow()
+        {
+            var g = new GameState();
+            g.MyHand.AddRange(MyHand);
+            g.OppKnown.AddRange(OppKnown);
+            g.Field.AddRange(Field);
+            g.MyPile.AddRange(MyPile);
+            g.OppPile.AddRange(OppPile);
+            return g;
+        }
     }
 }
